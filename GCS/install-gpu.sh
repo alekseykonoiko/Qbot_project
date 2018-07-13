1eecf7534c28d34d9f189b11aeac422c75a8fca8
Aleksey Konoiko <lesha.konoiko@gmail.com>
2018-07-13 13:00:41 +0100

    misc

--- GCS/install-gpu.sh
+++ GCS/install-gpu.sh
@@ -30,6 +30,6 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install nvidia-docker2
-sudo pkill -SIGHUP dockerd
+sudo apt-get -y update
+sudo apt-get -y install nvidia-docker2
+sudo pkill -y -SIGHUP dockerd
