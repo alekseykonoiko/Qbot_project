4fb9ab9582c75afce841d6373f43697ffb3e4750
Aleksey Konoiko <lesha.konoiko@gmail.com>
2018-07-13 13:41:33 +0100

    misc

--- GCS/install-gpu.sh
+++ GCS/install-gpu.sh
@@ -25,11 +25,26 @@ sudo apt-get install -y docker-ce
 sudo docker run hello-world #verify docker installation
 
 ## Install nvidia-docker2 ##
+# curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+#   sudo apt-key add -
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+#   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+# sudo apt-get -y update
+# sudo apt-get -y install nvidia-docker2
+# sudo pkill -y -SIGHUP dockerd
+
+
+docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
+sudo apt-get purge nvidia-docker
+
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
   sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
   sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get -y update
+
 sudo apt-get -y install nvidia-docker2
-sudo pkill -y -SIGHUP dockerd
+
+sudo pkill -SIGHUP -y dockerd
