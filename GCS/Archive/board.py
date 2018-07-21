28af77adcaeb6af68ed18c92e1c4af486ccbd124
Aleksey Konoiko <lesha.konoiko@gmail.com>
2018-07-21 15:45:48 +0100

    Restructured files, cleaned legacy scripts

--- GCS/Archive/board.py
+++
@@ -1,8 +0,0 @@
-import keras
-
-def createTensorboardConfig(pathToLogs):
-    return keras.callbacks.TensorBoard(
-        log_dir=pathToLogs,
-        histogram_freq=0,
-        write_graph=True,
-        write_images=False)
