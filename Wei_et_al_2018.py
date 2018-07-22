326efd4437308dd3fc630989e83bf29a32bf3b08
Sion93 <40235258+Sion93@users.noreply.github.com>
2018-07-22 09:57:35 +0100

    new structure

--- Wei_et_al_2018.py
+++ Wei_et_al_2018.py
@@ -34,17 +34,19 @@ y_test = npzfile['y_test']
 
 #Pavia University
 
+#Salinas Scene
+
 
 # Data Parameters
 # h= height 
 # w= width
 # d= depth
-# s= number of samples
+# s= number of samples #200
 
 #### Hyperparameters ####
 batch_size = 10
-num_classes = 8
-epochs = 1
+num_classes = K #9
+epochs = 1 #100
 
 #### CNN structure (Functional API Model Style) ####
 
@@ -63,39 +65,30 @@ initializer = keras.initializers.RandomNormal(mean=0.0, stddev=0.005, seed=True)
 #initializer = keras.initializers.he_uniform(seed=None)
 
 
-input1 = Input(shape=(32,32,288))
+input1 = Input(shape=(2*3,3,103,1)) #input is normalised from 0 t 1
 
-spatial = Conv2D(128, (1, 1), padding='same', activation='relu')(input1)
-spatial= Conv2D(128, (3, 3), padding='same', activation='relu')(spatial)
+# spatial = Conv2D(128, (1, 1), padding='same', activation='relu')(input1)
+# spatial= Conv2D(128, (3, 3), padding='same', activation='relu')(spatial)
 
-spectral = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(input1)
-spectral = Conv2D(64, (1, 1), padding='same', activation='relu')(spectral)
+# spectral = MaxPooling2D((3, 3), strides=(1, 1), padding='same')(input1)
+# spectral = Conv2D(64, (1, 1), padding='same', activation='relu')(spectral)
 
-concat = concatenate([spatial, spectral], axis=3)
+# concat = concatenate([spatial, spectral], axis=3)
 
-l1 = Conv2D(128, kernel_size=(1, 1), padding='same', activation='relu')(concat)
-l1bn=BatchNormalization()(l1)
-l2 = Conv2D(128, kernel_size=(1, 1), padding='same', activation='relu')(l1bn)
-l2bn=BatchNormalization()(l2)
-l3 = Conv2D(128, kernel_size=(1, 1), padding='same', activation='relu')(l2bn)
-add1 = add([l1bn, l3])
+l1 = Conv3D(6, kernel_size=(1, 1, 1), strides=(1, 1, 1), padding='same', activation='relu')(input1)
+l2 = Conv3D(6, kernel_size=(3, 1, 8), strides=(1, 1, 3), padding='same', activation='relu')(l1)
+l3 = Conv3D(12, kernel_size=(1, 2, 3), strides=(1, 1, 1), padding='same', activation='relu')(l2)
+l4 = Conv3D(24, kernel_size=(3, 1, 3), strides=(1, 1, 2), padding='same',activation='relu')(l3)
+l5 = Conv3D(48, kernel_size=(2, 1, 3), strides=(1, 1, 1), padding='same',activation='relu')(l4)
+l6 = Conv3D(48, kernel_size=(1, 2, 3), strides=(1, 1, 2), padding='same', activation='relu')(l5)
+l7 = Conv3D(96, kernel_size=(1, 1, 3), strides=(1, 1, 1), padding='same', activation='relu')(l6)
+l8 = Conv3D(96, kernel_size=(1, 1, 3), strides=(1, 1, 1), padding='same', activation='relu')(l7)
+l9 = Conv3D(10, kernel_size=(1, 1, 1), strides=(1, 1, 1), padding='same', activation='relu')(l8)
 
-
-l4 = Conv2D(128, kernel_size=(1, 1), padding='same',activation='relu')(add1)
-l5 = Conv2D(128, kernel_size=(1, 1), padding='same',activation='relu')(l4)
-add2 = add([l4, l5])
-
-l7 = Conv2D(128, kernel_size=(1, 1), activation='relu')(add2)
-drop1 = SpatialDropout2D(0.2)(l7)
-l8 = Conv2D(128, kernel_size=(1, 1), activation='relu')(drop1)
-drop2 = SpatialDropout2D(0.2)(l8)
-l9 = Conv2D(128, kernel_size=(1, 1), activation='relu')(drop2)
-
-flat = Flatten()(l9)
-output = Dense(num_classes, activation='softmax')(flat)
+output = Conv3D(K, kernel_size=(K+1, 1, 1), padding='same', activation='softmax')(l9) # output size C+1 *1*1
 model = Model(inputs=input1, outputs=output)
 
-## initiate RMSprop optimizer
+## initiate optimizer
 
 opt0 = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=5e-4)
 
@@ -103,11 +96,9 @@ opt1 = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, d
 
 opt2 = keras.optimizers.SGD(lr=0.001, momentum=0.9, decay=0.0005, nesterov=False)
 
-## Let's train the model using RMSprop
-
 model.compile(loss='categorical_crossentropy', 
               
-              optimizer=opt2, 
+              optimizer=opt1, 
               
               metrics=['accuracy'])
 ## Scale 0-255 bands range into float 0-1
